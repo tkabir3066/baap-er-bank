90c1a3bfd2f4875abfeba7b4799d77cbb88df785
Tutul Kabir <tutulkabir@gmail.com>
2023-08-21 15:03:38 +0530

    added event listener with submit button to get email and password

---
+++ js/login.js
@@ -0,0 +1,23 @@
+const btnSubmit = document.getElementById("btn-submit");
+
+//step-1: add click handler with the submit button
+btnSubmit.addEventListener("click", () => {
+  // step-2: get the email address inside the email input field
+  // always remember to use .value to get text from an input field
+  const emailField = document.getElementById("user-email");
+  const email = emailField.value;
+
+  //step-3: get the password
+
+  const passwordField = document.getElementById("password");
+  const password = passwordField.value;
+
+  // Danger : DO NOT VERIFY EMAIL PASSWORD ON THE CLIENT SIDE
+
+  //   STEP-4 : VERIFY EMAIL AND PASSWORD and check whether valid user or not
+  if (email === "sontan@gmail.com" && password === "secret") {
+    console.log("Valid user");
+  } else {
+    console.log("Invalid user");
+  }
+});
